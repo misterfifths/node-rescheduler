77b2258d9c7155b2a165f141ccfb492b5eee50f5
Tim Clem <tim.clem@gmail.com>
2014-01-26 20:21:32 -0500

    Docs & cleanup & remove _ dep

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 var events = require('events'),
     util = require('util'),
-    _ = require('underscore'),
     luaScript;
 
 
@@ -11,36 +10,70 @@ var events = require('events'),
 // We store payloads in a sorted set, with their score set to the timestamp of when they're
 // supposed to be enqueued for processing. Then, every so often, we run this script, which
 // checks for any newly ready payloads (zrangebyscore with the current time as the maximum
-// score), pushes them onto the destination queue (rpush, which maintains the sort order
+// score), pushes them onto the destination list (rpush, which maintains the sort order
 // from the sorted set), and then removes them from the set (zremrangebyscore).
 // Since scripts are run atomically by Redis, we don't need to do a piecemeal zrem for
 // each of the payloads; the set cannot have changed between the zrangebyscore and
 // zremrangebyscore calls.
-// So, as a script, this takes 2 keys (the zset and the destination queue), and
+// So, as a script, this takes 2 keys (the zset and the destination list), and
 // one argument, the maximum timestamp to enqueue.
-luaScript = [ 'local winners = redis.call("zrangebyscore", KEYS[1], 0, ARGV[1])',
-              'if #winners == 0 then return 0 end',
+// Doing this via the Lua interface saves us having to needlessly pull down all the values
+// we're dealing with, only to push them back up to Redis and discard them -- this way
+// the only value returned to us is an integer. Yay!
+luaScript = [ 'local winners = redis.call("zrangebyscore", KEYS[1], 0, ARGV[1])',  // note that arrays are 1-based in Lua
+              'if #winners == 0 then return 0 end',  // the # operator gives the length of an array
 
-              'redis.call("rpush", KEYS[2], unpack(winners))',
+              'redis.call("rpush", KEYS[2], unpack(winners))',  // unpack turns an array into 1-by-1 arguments (akin to function.apply() in JS)
               'redis.call("zremrangebyscore", KEYS[1], 0, ARGV[1])',
               'return #winners'
             ].join('\n');
 
 
-function ReScheduler(redisClient, targetQueue, options) {
-    var self = this;
+// An object that handles enqueuing values for future processing in Redis.
+//
+// redisClient (created via redis.createClient()) must be dedicated solely to this instance; no other
+// operations should be performed on it until this object is shut down.
+//
+// targetList is the name of the Redis list onto which values will be pushed when it is time for
+// them to be executed. They will be pushed onto this list using the 'rpush' command. Consumption
+// of values whose time has come is intended to happen via a simple 'lpop' or 'blpop' on the
+// given list. The pop() method of this class is just a wrapper for 'blpop'; another module
+// could easily consume values without any knowledge of ReScheduler.
+//
+// options is an optional hash. At present, the only option is 'checkInterval', the number of 
+// milliseconds between checks for values whose execution time has arrived. It defaults to
+// one minute. Provide a value of 0 to disable automatic checking; if you do this, it is up to
+// you to intermittently call checkNow() to process values.
+//
+// Note that the accuracy of when a value is enqueued is entirely dependent on how often
+// checkNow() is called, either via checkInterval or manually. If you need high-resolution
+// (e.g. less than a second), this is not the class for you.
+//
+// If automatic checking is enabled, you may call stopChecking() to cancel it.
+// If an error occurs while the automatic checking is running, the 'error' event will be emitted
+// with the related Error object.
+//
+// Like promises? Call qWrap() and all callback functions will return promises instead.
+function ReScheduler(redisClient, targetList, options) {
+    var self = this,
+        key;
+
     function defineReadonlyProperty(name, value, enumerable) {
         Object.defineProperty(self, name, { value: value, enumerable: enumerable !== false });
     }
 
     defineReadonlyProperty('client', redisClient);
-    defineReadonlyProperty('targetQueue', targetQueue);
-    defineReadonlyProperty('zsetName', targetQueue + '-scheduler');
+    defineReadonlyProperty('targetList', targetList);
+    defineReadonlyProperty('zsetName', targetList + '-scheduler');
 
-    options = options ? _.clone(options) : {};
-    _.defaults(options, {
-        checkInterval: 60 * 1000
-    });
+    var _options = { checkInterval: 60 * 1000 };
+    for(key in _options) {
+        if(options.hasOwnProperty(key) && options[key] !== undefined)
+            _options[key] = options[key];
+    }
+
+    options = _options;
+    console.log(options);
     defineReadonlyProperty('options', Object.freeze(options));
 
     if(this.options.checkInterval > 0) {
@@ -64,6 +97,10 @@ module.exports = ReScheduler;
 util.inherits(ReScheduler, events.EventEmitter);
 
 ReScheduler.prototype = Object.create(ReScheduler.prototype, {
+    // Enqueues payload for processing at the given date (a Date object, or a timestamp like
+    // that from Date.getTime()).
+    // The callback is called with 0 if the payload is already awaiting its execution date (in
+    // which case the date is updated), or 1 if it is new.
     enqueueAt: {
         value: function(date, payload, callback) {
             var timestamp = (typeof date === 'number') ? date : date.getTime();
@@ -72,6 +109,8 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
         writable: true
     },
 
+    // Same as enqueueAt, except with a relative time -- the given number of minutes from
+    // the current time.
     enqueueIn: {
         value: function(minutes, payload, callback) {
             this.enqueueAt((new Date()).getTime() + minutes * 60 * 1000, payload, callback);
@@ -79,6 +118,7 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
         writable: true
     },
 
+    // Calls back with the number of elements awaiting their execution date.
     scheduledCount: {
         value: function(callback) {
             this.client.zcard(this.zsetName, callback);
@@ -86,16 +126,24 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
         writable: true
     },
 
+    // Looks for values whose execution time has come, and moves them onto targetList.
+    // Calls back with the number of values moved to targetList.
+    // This function is called automatically unless options.checkInterval was 0, or
+    // stopChecking() was called.
     checkNow: {
         value: function(callback) {
             var self = this,
                 maxScore = (new Date()).getTime();
 
-            this.client.eval([ luaScript, 2, this.zsetName, this.targetQueue, maxScore ], callback);
+            this.client.eval([ luaScript, 2, this.zsetName, this.targetList, maxScore ], callback);
         },
         writable: true
     },
 
+    // Pops the next ready-to-be-executed value off targetList.
+    // The callback is called with the value, or undefined if the timeout was reached with
+    // no values having been pushed to the list.
+    // timeout is optional and defaults to 0, which means "wait forever".
     pop: {
         value: function(client, timeout, callback) {
             if(typeof timeout == 'function') {
@@ -103,7 +151,7 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
                 timeout = 0;
             }
 
-            client.blpop([ this.targetQueue, timeout ], function(err, res) {
+            client.blpop([ this.targetList, timeout ], function(err, res) {
                 if(callback) {
                     if(err) callback(err);
                     else callback(null, res[1]);
@@ -113,6 +161,9 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
         writable: true
     },
 
+    // Stop any automatic checking and optionally close the Redis client we were given.
+    // If leaveClientOpen is true, you are free to use the client for other purposes
+    // after calling this function.
     shutdown: {
         value: function(leaveClientOpen) {
             if(this.stopChecking) this.stopChecking();
@@ -120,6 +171,13 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
         }
     },
 
+    // Wrap all callback functions in Q promises.
+    // You may optionally pass in the Q module, if "require('q')" will not provide the one
+    // you wish.
+    // After this function returns, all methods that take callbacks will return Q promises
+    // for their results. This function returns `this`; it modifies the object in-place.
+    // You may still call methods with a callback if you wish by simply pretending this
+    // function was never called :)
     qWrap: {
         value: (function() {
             var wrapped = false;
@@ -132,7 +190,7 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
                     if(!Q) Q = require('q');  // This will throw if q isn't installed, which is fine.
 
                     for(i = 0; i < cbFns.length; i++)
-                        wrapFunction(this, cbFns[i], Q);
+                        promisify(this, cbFns[i], Q);
 
                     wrapped = true;
                 }
@@ -143,7 +201,13 @@ ReScheduler.prototype = Object.create(ReScheduler.prototype, {
     }
 });
 
-function wrapFunction(rs, fnName, Q) {
+// Given an object and a function name on it, this rewrites that function such that if it's
+// called with one less argument than it expects, it returns a Q promise for the call.
+// If the function is called with the numer of arguments it expects, the original function
+// is called as normal.
+// That is: this makes fnName seamlessly deal with promises -- if you pass a callback, that will
+// work. If you don't, the function will give you back a promise.
+function promisify(rs, fnName, Q) {
     var originalFn = rs[fnName],
         originalArity = originalFn.length;
 
